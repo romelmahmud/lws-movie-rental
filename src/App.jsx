335579a97a348a0bec8d8b2f9cf918efa7e677b7
unknown <romelmahmud.dev@gmail.com>
2024-10-21 21:45:33 +0600

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-green-600 font-bold text-center mt-[100px]">
+        Hello from React + Vite + Tailwind Css
+      </h1>
+    </>
+  );
+}
+
+export default App;
