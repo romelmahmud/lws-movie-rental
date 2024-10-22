13b4da9a9020f68b55b4c7645ac24b58f8d60088
unknown <romelmahmud.dev@gmail.com>
2024-10-22 16:40:17 +0600

    basic setup complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.25rem",
+      },
+      colors: {
+        primary: "#00d991",
+      },
+    },
   },
   plugins: [],
 };
